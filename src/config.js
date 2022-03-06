1c806bb61ebe497ee238ed1c635a057936a24005
Chandru <chandru6501@gmail.com>
2022-03-06 15:57:35 +0530

    end

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API = process.env.REACT_APP_API_URL || "https://apicscommerce.herokuapp.com/";
+export const API = process.env.REACT_APP_API_URL || "https://apicscommerce.herokuapp.com";
